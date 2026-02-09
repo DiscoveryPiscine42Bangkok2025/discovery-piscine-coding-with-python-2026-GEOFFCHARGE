212b5bebc2e9666d12e6500b3307215870b0209d
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-02-10 00:48:40 +0700

    add cell02 ex03

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+first_number = int(input("Enter the first number:\n"))
+second_number = int(input("Enter the second number:\n"))
+result = first_number * second_number
+print(f"{first_number} x {second_number} = {result}")
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
