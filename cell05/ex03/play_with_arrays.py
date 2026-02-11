1afda529aef6a95a8524d83fc50f155485ee9c7f
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-02-11 18:55:42 +0700

    update cell05 ex03

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-original =  [2, 8, 9, 48, 8, 22, -12, 2]
+org = [2, 8, 9, 48, 8, 22, -12, 2]
 new = []
-for i in original:
+for i in org:
     if i > 5 and i + 2 not in new:
         new.append(i + 2)
-print(f"Original array: {original}")
+print(f"Original array: {org}")
 print(f"New array: {new}")
